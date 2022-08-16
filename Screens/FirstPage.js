e8a7470a5e1fa79297cd84ed381200954afbfab2
DISAWAT​ SRISAMRAN <58892343+smart11807@users.noreply.github.com>
2022-08-16 21:21:50 +0700

    newcommit

--- Screens/FirstPage.js
+++ Screens/FirstPage.js
@@ -1,44 +1,44 @@
-import {StyleSheet, View, Text, Button,TextInput } from 'react-native'
+import { StyleSheet, View, Text, Button, TextInput } from 'react-native'
 import React from 'react'
 
-const FirstPage = ({navigation,route}) => {
+const FirstPage = ({ navigation, route }) => {
 
-const [postText, setPostText] = React.useState('');
+    const [postText, setPostText] = React.useState('');
 
-  return (
-    <View style={styles.container}>
-      <Text style={styles.heading}>Thai-Nichi Institute of Technology</Text>
-      <Text style={styles.textStyle}>Please insert your name to pass it to second screen</Text>
-      <TextInput
-      placeholder='your name'
-      style={{ height: 50, padding: 10,marginBottom:10, backgroundColor: '#fff' }}
-      onChangeText={setPostText}
-      value={postText}
-      />
-      <Button
-      title='go next'
-      onPress={() => navigation.navigate('SecondPage',{postText})}
-      />
-    </View>
-  )
+    return (
+        <View style={styles.container}>
+            <Text style={styles.heading}>Thai-Nichi Institute of Technology</Text>
+            <Text style={styles.textStyle}>Please insert your name to pass it to second screen</Text>
+            <TextInput
+                placeholder='your name'
+                style={{ height: 50, padding: 10, marginBottom: 10, backgroundColor: '#fff' }}
+                onChangeText={setPostText}
+                value={postText}
+            />
+            <Button
+                title='go next'
+                onPress={() => navigation.navigate('SecondPage', { postText })}
+            />
+        </View>
+    )
 }
 
 export default FirstPage
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    padding: 20,
-  },
-  heading: {
-    fontSize: 25,
-    textAlign: 'center',
-    marginVertical: 10,
-  },
-  textStyle: {
-    textAlign: 'center',
-    fontSize: 16,
-    marginVertical: 10,
-  },
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        padding: 20,
+    },
+    heading: {
+        fontSize: 25,
+        textAlign: 'center',
+        marginVertical: 10,
+    },
+    textStyle: {
+        textAlign: 'center',
+        fontSize: 16,
+        marginVertical: 10,
+    },
 });
