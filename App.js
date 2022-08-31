2aa446758732b2484ed1ec36cdcbb6b50bb4741e
smart11807 <smart.207077@gmail.com>
2022-08-31 11:25:41 +0700

    newcommit

--- App.js
+++ App.js
@@ -1,37 +1,67 @@
 import * as React from "react";
-import { Button, View } from "react-native";
-import { createDrawerNavigator } from "@react-navigation/drawer";
+import { Button, View, Text } from "react-native";
+import {
+  createDrawerNavigator,
+  DrawerContentScrollView,
+  DrawerItemList,
+  DrawerItem,
+} from "@react-navigation/drawer";
 import { NavigationContainer } from "@react-navigation/native";
 
-function HomeScreen({ navigation }) {
+function Feed({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      {" "}
-      <Button
+      {/* <Button
         onPress={() => navigation.navigate("Notifications")}
         title="Go to notifications"
-      />{" "}
+      /> */}
+      <Text>Feed Screen</Text>
     </View>
   );
 }
 
-function NotificationsScreen({ navigation }) {
+function Article({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Button onPress={() => navigation.goBack()} title="Go back home" /> {" "}
+      {/* <Button onPress={() => navigation.goBack()} title="Go back home" /> */}
+      <Text>Article Screen</Text>
     </View>
+  );
+}
+
+function CustomDrawerContent(props) {
+  return (
+    <DrawerContentScrollView {...props}>
+      <DrawerItemList {...props} />
+      <DrawerItem label="Help" onPress={() => alert("Link to help")} />
+    </DrawerContentScrollView>
   );
 }
 
 const Drawer = createDrawerNavigator();
+
+function MyDrawer() {
+  return (
+    <Drawer.Navigator
+      useLegacyImplementation
+      drawerContent={(props) => <CustomDrawerContent {...props} />}
+      screenOptions={{
+        drawerStyle:{
+          backgroundColor:'lightblue',
+          Width:240,
+        }
+      }}
+    >
+      <Drawer.Screen name="Feed" component={Feed} />
+      <Drawer.Screen name="Article" component={Article} />
+    </Drawer.Navigator>
+  );
+}
+
 export default function App() {
   return (
     <NavigationContainer>
-      {" "}
-      <Drawer.Navigator useLegacyImplementation>
-        <Drawer.Screen name="Home" component={HomeScreen} />{" "}
-        <Drawer.Screen name="Notifications" component={NotificationsScreen} /> {" "}
-      </Drawer.Navigator>{" "}
+      <MyDrawer />
     </NavigationContainer>
   );
 }
