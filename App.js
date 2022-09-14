940510b893180768745041d1144ae6bd47586fe8
smart11807 <smart.207077@gmail.com>
2022-09-14 10:30:48 +0700

    newcommit

--- App.js
+++ App.js
@@ -1,81 +1,14 @@
-import { View, Text, Button } from "react-native";
-import React from "react";
-import { Ionicons } from "@expo/vector-icons";
-import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { createDrawerNavigator } from "@react-navigation/drawer";
-
-const myTheme = {
-  ...DefaultTheme,
-  colors: {
-    ...DefaultTheme.colors,
-    primary: "rgb(255,45,85)",
-  },
-};
-
-const Tab = createBottomTabNavigator();
-const Drawer = createDrawerNavigator();
-
-function HomeScreen({ navigation }) {
-  return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Home</Text>
-      <Button
-        title="Go to setting"
-        onPress={() => navigation.navigate("Setting")}
-      />
-    </View>
-  );
-}
-function SettingScreen({ navigation }) {
-  return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Setting</Text>
-      <Button title="Go to home" onPress={() => navigation.navigate("Home")} />
-    </View>
-  );
-}
-
-function MyTab() {
-  return (
-    <Tab.Navigator
-      screenOptions={({ route }) => ({
-        tabBarIcon: ({ focused, color, size }) => {
-          let iconName;
-          if (route.name === "Home") {
-            iconName = focused
-              ? "ios-information-circle"
-              : "ios-information-circle-outline";
-          } else if (route.name === "Setting") {
-            iconName = focused ? "ios-list-box" : "ios-list";
-          }
-          return <Ionicons name={iconName} size={size} color={color} />;
-        },
-        tabBarActiveTintColor: "tomato",
-        tabBarInactiveTintColor: "gray",
-      })}
-    >
-      <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="Setting" component={SettingScreen} />
-    </Tab.Navigator>
-  );
-}
-
-function MyDrawer() {
-  return (
-    <Drawer.Navigator useLegacyImplementation >
-      <Drawer.Screen name="Home" component={MyTab} />
-      <Drawer.Screen name="Setting" component={SettingScreen} />
-    </Drawer.Navigator>
-  );
-}
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
 
 const App = () => {
   return (
-    <NavigationContainer theme={myTheme}>
-      <MyDrawer />
-    </NavigationContainer>
-  );
-};
+    <View>
+      <Text>App</Text>
+    </View>
+  )
+}
 
-export default App;
+export default App
+
+const styles = StyleSheet.create({})
